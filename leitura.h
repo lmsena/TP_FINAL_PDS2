46975407d8741a60f074fc622a161a77cfad0d72
leonardo sena <42551215+lmsena@users.noreply.github.com>
2019-06-05 00:26:19 -0300

    Update leitura.h

--- leitura.h
+++ leitura.h
@@ -1,10 +1,12 @@
-#ifndef LEITURA_H
 #define LEITURA_H
 #include <string>
 #include <fstream>
 #include<list>
 #include<vector>
+#include<set>
 #include<map>
+#include <iostream>
+#include<algorithm>
 using namespace std;
 
 class Leitura{
@@ -15,7 +17,12 @@ class Leitura{
         void ReadFile(int qtdArquivos);
         void Tudominusculo(string&word);
         void TiraCaracter(string&word);
+        //Retorna o numero de vezes que uma palavra ocorre em um arquivo
         int TermFrequency(string word,vector< list<string> > palavras,int i);
+        //Retorna true se uma palavra pertence a um arquivo
+        bool Pertence(string word);
+        //Insere uma palavra no Map com seus respectivos arquivos
+        void InsereMap(string word);
         //~Leitura();
         //
 
@@ -29,12 +36,11 @@ class Leitura{
         vector <string> nomeArquivo;
         int quantidadeArquivos;
         list<string>listaPalavras;
+        map <string, set<string> >palavras;
 
 
 
 };
-
-
 
 
 
