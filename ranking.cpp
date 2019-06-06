3c64710d27c89a43835b1c52ff446640427867c2
leonardo sena <42551215+lmsena@users.noreply.github.com>
2019-06-06 14:29:50 -0300

    Update ranking.cpp

--- ranking.cpp
+++ ranking.cpp
@@ -1,5 +1,13 @@
-double Ranking::idf()
+#include "ranking.h"
+
+
+double Ranking::idf(string wordIDF)
 {
-    idf = log2(quantidadeArquivos / this->palavras->second.size());
+   list<string>x;
+   x = palavras[wordIDF];
+   // x = palavras.at(wordIDF).second;
+   idf = log2(quantidadeArquivos / this->palavras->second.size());
+   idf = log2(quantidadeArquivos / x.second);
+
     return idf;
 }
