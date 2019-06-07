613b8dc8499b1fa8ce846a8c15db30ffc2fde0bc
leonardo sena <42551215+lmsena@users.noreply.github.com>
2019-06-07 15:33:10 -0300

    Create indice.h

---
+++ indice.h
@@ -0,0 +1,31 @@
+#ifndef INDICE_H
+#define INDICE_H
+#include <string>
+#include <fstream>
+#include<list>
+#include<vector>
+#include<set>
+#include<map>
+using namespace std;
+
+class Indice{
+
+public:
+    double idf(string wordIDF);
+    bool Pertence(string word);
+    void Tudominusculo(string&word);
+    void TiraCaracter(string&word);
+    int TermFrequency(string word,vector< list<string> >palavras,int i);
+    //armazena as palavras de todos os documentos ou seja o list de cada arquivo
+    vector < list<string> > palavrasDocs;
+    //armazena as palavras de um unico arquivo
+    list <string> wordFile;
+    vector <string> nomeArquivo;
+    int quantidadeArquivos;
+    list<string>listaPalavras;
+    map <string, set<string> >palavras;
+
+    friend class Leitura;
+    friend class Ranking;
+};
+#endif
