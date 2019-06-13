920ca9915be60453db8edb0bb39c1e0389300179
leonardo sena <42551215+lmsena@users.noreply.github.com>
2019-06-13 12:46:52 -0300

    Update leitura.cpp

--- leitura.cpp
+++ leitura.cpp
@@ -33,7 +33,7 @@ void Leitura::ReadFile(int quantidadeArquivos,Indice &indice){
             string word;
             arquivo_>>word;
             indice.Tudominusculo(word);
-            //j� insere a palavra lida e manipulada na lista de palavras do documento
+            //já insere a palavra lida e manipulada na lista de palavras do documento
             wordFile.push_back(word);
             //inserindo a palavra em um Map que relaciona  quais arquivos contem a palavra
             if(!indice.Pertence(word)){
@@ -65,4 +65,69 @@ void Leitura::ReadFile(int quantidadeArquivos,Indice &indice){
         exit(2);
     }
 }
+
+
+//contrutor p teste
+Leitura::Leitura(int qtdArquivos, bool teste){
+    quantidadeArquivos=qtdArquivos;
+    Indice I();
+    ReadFileTeste(quantidadeArquivos, &I);
+    
+
+}
+
+
+
+//Le os arquivos para teste
+void Leitura::ReadFileTeste(int quantidadeArquivos,Indice &indice){
+
+
+    nomeArquivo.push_back("d1.txt");
+    nomeArquivo.push_back("d2.txt");
+    nomeArquivo.push_back("d3.txt");
+    nomeArquivo.push_back("d4.txt");
+    list<string>wordFile;
+
+
+//Lendo e manipulando as palavras de cada arquivo
+    for(int i=0;i<quantidadeArquivos;i++){
+        arquivo_.open(nomeArquivo[i].c_str(),std::ios::in);
+        if(arquivo_.good()){
+      while(!arquivo_.eof()){
+            string word;
+            arquivo_>>word;
+            indice.Tudominusculo(word);
+            //já insere a palavra lida e manipulada na lista de palavras do documento
+            wordFile.push_back(word);
+            //inserindo a palavra em um Map que relaciona  quais arquivos contem a palavra
+            if(!indice.Pertence(word)){
+                set<string>indicePalavras;
+                indicePalavras.insert(nomeArquivo[i]);
+                indice.palavras[word]=indicePalavras;
+            }
+            else{
+                set<string>indicePalavrasRepetidas=indice.palavras[word];
+                indicePalavrasRepetidas.insert(nomeArquivo[i]);
+                indice.palavras[word]=indicePalavrasRepetidas;
+            }
+
+        }
+    }
+        else{
+            cout<<"Nao foi possivel abrir o arquivo "<<nomeArquivo[i]<<endl;
+            nomeArquivo.erase(nomeArquivo.begin()+i);
+            quantidadeArquivos--;
+            i--;
+        }
+        indice.palavrasDocs.push_back(wordFile);
+        wordFile.clear();
+        arquivo_.close();
+
+    }
+    if(quantidadeArquivos==0){
+        cout<<"Nao foi possivel abrir nenhum arquivo"<<endl;
+        exit(2);
+    }
+}
+
 
